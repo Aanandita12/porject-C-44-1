240a57ead7e08939092d97cfe340965c8014f586
Aanandita12 <79647330+Aanandita12@users.noreply.github.com>
2021-07-12 16:06:37 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,31 @@
+var abaus
+
+/*function preload(){
+  bg=loadImage(picher5.PNG);
+  
+}*/
+
+function setup() {
+  createCanvas(800,400);
+  createSprite(670,150, 200, 60);
+
+  abaus=createSprite(90,150,100,60);
+  //abaus.shapeColor("white");
+
+}
+
+
+
+function draw() {
+  background("green");
+   
+ 
+ if  
+  drawSprites();
+}
+
+/*function clues(){
+  Fill("Red");
+  textSize(70);
+  text("Counting");
+}*/
